2fac0cbf7599e73656e396d2c43f3d4939f54363
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2012-12-04 20:55:24 +0100

    add more quotes

--- src/main/java/jenkins/plugins/johnny/FactGenerator.java
+++ src/main/java/jenkins/plugins/johnny/FactGenerator.java
@@ -25,46 +25,76 @@ import java.util.Random;
 
 /**
  * {@link FactGenerator} provides Chuck Norris facts.
+ * 
  * @author cliffano
+ * @author Nicolas François
  */
 public class FactGenerator {
 
-    /**
-     * The configured Chuck Norris facts, from <a href="http://www.codesqueeze.com/the-ultimate-top-25-chuck-norris-the-programmer-jokes/"
-     * >http://www.codesqueeze.com/the-ultimate-top-25-chuck-norris-the-
-     * programmer-jokes/</a>.
-     */
-    private static final String[] FACTS = {
-            "Je n'ai jamais cherché le pourquoi du comment...",
-            "Pour faire un couple, il faut être deux.",
-            "Si je dois dix centimes au fisc, on vient me les saisir. Un jour, les huissiers ont même emporté mes blousons",
-            "Je suis souvent seul dans la vie, mais sur scène jamais... Ma drogue à moi, c'est le public.",
-            "Je vis avec moi-même tous les jours",
-            "Si on avait pas perdu une heure, on serait arrivés une heure plus tôt.",
-            "La poudre et le hasch circulent à mort chez les musiciens. Il n'y a pas à s'en vanter, je n'en suis pas fier, c'est ainsi, c'est tout. Mais il faut bien savoir que nos chansons, on ne les sort pas forcément d'une pochette-surprise.",
-            "A Los Angeles, je peux acheter mes sauces piquantes sans qu'on se jette sur moi, de temps en temps ça fait du bien ! ",
-            "Il n'y a pas de honte à faire de la pub, ca aide à payer les réparations du toit de la maison ! ",
-            "En voyant le public, j'oublie que j'ai mal",
-            "Les gens m'appellent l'idole des jeunes...",
-            "Je ne suis jamais seul, à moins que je lis.",
-            "Je vis au jour le jour, pour demain, pas pour hier.",
-            "J’aimerais apprendre à dire non pour ne plus me retrouver à dire oui sans faire attention.",
-            "Est-ce que vous les connaissez ces joueurs de l’équipe de France de Football ? – Oui, je connais Zazie évidemment…",
-            "Ma devise est : exister, c'est insister !",
-            "Ce qui rend heureux, c'est d'être heureux.",
-            "C'est Jésus-Christ, le premier héros de ma jeunesse. Il arrivait à attirer les foules !"
-    };
+	/**
+	 * The configured Johnny quotes.
+	 */
+	private static final String[] FACTS = {
+			"Je n'ai jamais cherché le pourquoi du comment...",
+			"Pour faire un couple, il faut être deux.",
+			"Si je dois dix centimes au fisc, on vient me les saisir. Un jour, les huissiers ont même emporté mes blousons",
+			"Je suis souvent seul dans la vie, mais sur scène jamais... Ma drogue à moi, c'est le public.",
+			"Je vis avec moi-même tous les jours",
+			"Si on avait pas perdu une heure, on serait arrivés une heure plus tôt.",
+			"La poudre et le hasch circulent à mort chez les musiciens. Il n'y a pas à s'en vanter, je n'en suis pas fier, c'est ainsi, c'est tout. Mais il faut bien savoir que nos chansons, on ne les sort pas forcément d'une pochette-surprise.",
+			"A Los Angeles, je peux acheter mes sauces piquantes sans qu'on se jette sur moi, de temps en temps ça fait du bien ! ",
+			"Il n'y a pas de honte à faire de la pub, ca aide à payer les réparations du toit de la maison ! ",
+			"En voyant le public, j'oublie que j'ai mal",
+			"Les gens m'appellent l'idole des jeunes...",
+			"Je ne suis jamais seul, à moins que je lis.",
+			"Je vis au jour le jour, pour demain, pas pour hier.",
+			"J’aimerais apprendre à dire non pour ne plus me retrouver à dire oui sans faire attention.",
+			"Est-ce que vous les connaissez ces joueurs de l’équipe de France de Football ? – Oui, je connais Zazie évidemment…",
+			"Ma devise est : exister, c'est insister !",
+			"Ce qui rend heureux, c'est d'être heureux.",
+			"C'est Jésus-Christ, le premier héros de ma jeunesse. Il arrivait à attirer les foules !",
+			"J’ai des goûts très ecclésiastiques.",
+			"Pour les actes quotidiens, se réveiller ensemble, manger ensemble, c’est bien. C’est pas abstreignant.",
+			"Remettre les pendules à leur place",
+			"Pour faire un couple, il faut être deux",
+			"Je n'ai jamais dit «Ah que» dans une phrase, je vous le promets.",
+			"Ma devise est : exister, c'est insister ",
+			"Je me méfie des gens trop heureux : ils doivent avoir des choses à cacher.",
+			"Je ne suis en fait qu'un vieux cow-boy fatigué de son cheval et du désert.",
+			"Je ne vis pas en tant que Johnny Hallyday mais en tant que Jean-Philippe Smet.",
+			"Malgré les apparences, les chauffeurs, les secrétaires, les gardes du corps, les managers qui sont tout le temps à vos côtés quand vous êtes chanteur.",
+			"Clara Morgane, je ne vois pas trop qui c'est...",
+			"Jouer un gangster, c'est toujours amusant. On tue plein de Chinois.",
+			"De toute façon, je ne peux pas qualifier de fortune l'argent que j'ai. J'ai des maisons, c'est tout.",
+			"Pour moi, le Christ c'est un rockeur qu'on crucifie.",
+			"Je ne peux pas chanter n'importe quoi.",
+			"Lara Fabian, c'est moi en femme. Sur scène, on dirait qu'elle fait 2,20 m. Dès qu'elle sort, on voit qu'elle a un coeur de pomme.",
+			"J’ai du mal à m’imaginer écouter Johnny Hallyday. Je vis avec !",
+			"Jacques Chirac ? Un type droit, humain, qui sait renvoyer l’ascenseur. Il est venu dîner à la maison. Sans gardes du corps. Serein, tranquille. Eh ben ! Je vais vous dire : il raconte des histoires grivoises pas mal du tout.",
+			"Je ne serai pas un chanteur pathétique.",
+			"Je n’ai jamais triché avec mon public. J’ai toujours chanté en direct.",
+			"La cocaïne, j’en ai pris en tombant du lit. Maintenant, c’est fini. J’en prends pour travailler, relancer la machine. Je n’en suis pas fier. C’est ainsi, c’est tout.",
+			"La médiocrité, je ne supporte pas. L’admiration, c’est important pour le respect mutuel. Je ne pourrais pas vivre avec une conne.",
+			"Le rock, ça m’est venu quand j’ai entendu Loving You d’Elvis Presley. Je ne connaissais pas le rock’n roll à cette époque, je ne chantais que du Brassens. Quand je l’ai vu, je me suis dit : « C’est ça ce que je veux faire ! »",
+			"Moi, tout Johnny Hallyday que je suis, je rame à chaque nouvel album, à chaque nouveau concert. Pour que ça marche, il faut aller au baston.",
+			"Pour bien faire l’amour avec une femme, il faut l’avoir fait plusieurs fois. C’est toujours très mauvais la première fois !",
+			"Quelques fois, j'aimerais être con.",
+			"Je suis un intello (...) j’aurais pu être psy.",
+			"Au théâtre, pendant une représentation du Paradis sur terre, je me suis cassé un doigt sans même m’en rendre compte. J’ai commencé à avoir mal quand le rideau est tombé. Je m’oublie totalement, corps et âme.",
+			"Si Laeticia et moi on se sépare un jour, ce ne sera pas moi qui la quitterai. Ou plutôt, je sais que je ne la quitterai jamais pour une autre. Et puis, si c’est elle qui me quitte, eh bien, je sais aussi que je ne me remarierai jamais.",
+	};
 
-    /**
-     * Random instance.
-     */
-    private static final Random RANDOM = new Random();
+	/**
+	 * Random instance.
+	 */
+	private static final Random RANDOM = new Random();
 
-    /**
-     * Retrieves a random fact.
-     * @return a random fact
-     */
-    public String random() {
-        return FACTS[RANDOM.nextInt(FACTS.length)];
-    }
+	/**
+	 * Retrieves a random fact.
+	 * 
+	 * @return a random fact
+	 */
+	public String random() {
+		return FACTS[RANDOM.nextInt(FACTS.length)];
+	}
 }
